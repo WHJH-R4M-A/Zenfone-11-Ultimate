8de7e386912aa7f098d234f712bd50409331bcf3
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-06-10 13:08:04 +0800

    增加註解以及燈號顯示功能

--- main.ts
+++ main.ts
@@ -1,6 +1,112 @@
-input.onButtonPressed(Button.A, function () {
-	
+// 8上升
+// 2下降
+// 4向左轉
+// 6向右轉
+// 1上升C
+// 3下降E
+// 7向左轉F
+// 9向右轉D
+// 0停止
+// 
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 8) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 4) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . . . .
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 6) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            . . . . #
+            . . . # .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . . . . .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 7) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # . . . #
+            . . . # .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 9) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . . . #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . # # # .
+            # # . . #
+            # . # . #
+            # . . # #
+            . # # # .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
 })
-basic.forever(function () {
-	
-})
+radio.setGroup(1)
